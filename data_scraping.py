79ecfe5cc10097105a6a987f2ae2e15e04b08c40
Spencer <slh3mm@virginia.edu>
2024-11-12 20:30:46 -0500

    Updated scraping

--- data_scraping.py
+++ data_scraping.py
@@ -1,47 +1,70 @@
 import os
 import time
-from dotenv import load_dotenv; load_dotenv();
+from dotenv import load_dotenv
+
+from selenium.webdriver.common.keys import Keys
 from seleniumbase import SB
-from seleniumbase import BaseCase
-BaseCase.main(__name__, __file__)
+from selenium.webdriver.common.action_chains import ActionChains
+
+load_dotenv()
+
 
 # Initialize Chrome Session
-with SB(uc=True, test=True, incognito=True, locale_code="en") as sb:
+with SB(uc=True, incognito=True, locale_code="en") as sb:
     # Go to geoguessr.com, enter login info, bypass captcha, click login
-    sb.driver.uc_open_with_reconnect("https://www.geoguessr.com/signin")
-    sb.uc_gui_click_captcha()
-    sb.type('[data-qa="email-field"]', os.environ["UNAME"])
-    sb.type('[data-qa="password-field"]', os.environ["PWORD"])
-    sb.reconnect(0.1)
-    sb.uc_click('[data-qa="login-cta-button"]', reconnect_time=4)
+    sb.uc_open_with_reconnect("https://www.geoguessr.com/signin")
+
+    # Not always needed for some reason
+    sb.driver.uc_gui_click_captcha()
+
+    # Enter login info automatically
+    # sb.type('[data-qa="email-field"]', os.environ["UNAME"])
+    # sb.type('[data-qa="password-field"]', os.environ["PWORD"])
+    # sb.uc_click('[data-qa="login-cta-button"]', reconnect_time=4)
+
+    # OR Enter login info manually if auto doesn't work
+    sb.uc_gui_press_keys("\t\t\t")
+    sb.uc_gui_press_keys(os.environ["UNAME"])
+    sb.uc_gui_press_keys("\t")
+    sb.uc_gui_press_keys(os.environ["PWORD"])
+    sb.uc_gui_press_keys("\n")
 
     # Redirect to game page, press play
+    # If either of these doesn't work then after logging in, add in sb.sleep() and try going to Classic->World->Play
+    
+    # Automatic
     sb.driver.get("https://www.geoguessr.com/maps/world/play")
-    sb.uc_click('[data-qa="start-game-button"]')
-
+    sb.driver.click('[data-qa="start-game-button"]')
+    
+    # Manual approach
+    # driver.uc_gui_press_keys("\t\t\t\t\t\t\t\n")
+    
+    sb.driver.sleep(19)
+        
     # Play N_GAMES games, each with 5 rounds
-    N_GAMES = 2
-    SAVE_RATE = 1
+    N_GAMES = 5000
+    SAVE_RATE = 5
     game_id_buffer = ""
     for game in range(N_GAMES):
         # Grab game url and game id
         game_url = sb.get_current_url()
         game_id = game_url.split("/")[-1]
-        game_id_buffer += game_id + "\n"
-        print(game_url)
-        
+        if game_id != "audio.html":
+            game_id_buffer += game_id + "\n"
+
         # Save game_id every SAVE_RATE games
         if game % SAVE_RATE == 0:
             with open("game_ids.txt", "a") as f:
                 f.write(f"{game_id_buffer}")
                 f.close()
             game_id_buffer = ""
-            
+
         # Play the 5 rounds of a game
         for round in range(5):
             sb.click('[data-qa="guess-map"]')
             sb.click('[data-qa="perform-guess"]')
             sb.click('[data-qa="close-round-result"]')
-                
+
         # Start new game
         sb.click('[data-qa="play-again-button"]')
+        sb.sleep(1.5)
